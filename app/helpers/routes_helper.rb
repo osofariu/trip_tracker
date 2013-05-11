1b069c412c6a23c29fe45e9c0c1864c7c53885b0
Ovi Sofariu <ovi@sofariu.com>
2013-05-11 16:52:57 -0400

    see driving time on routes list

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -1,5 +1,16 @@
 module RoutesHelper
+
   def calc_distance(from_place_name, to_place_name)
     return 257
+  end
+  
+  def make_route_id(trip, route)
+     return "rt#{trip.id}-#{route.id}"
+  end
+
+  def get_route_driving_time(route)
+    if route.notes && route.notes.match(/Driving Time/)
+      return route.notes.scan(/Driving Time.*$/)[0][14..-1]
+    end    
   end
 end
